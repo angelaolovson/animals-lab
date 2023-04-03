5af14ff228b3ca6f43ef018c3994c5cb484868e2
Angela Olovson <angela.olovson@gmail.com>
2023-04-03 17:22:32 -0400

    Set up connection and models

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router(); 
+const Animal = require("../models/animal")
+
+// Post
+router.post("/", async (req,res) => {
+    req.body.extinct = req.body.extinct === "on" ? true : false;
+    res.redirect("/animals")
+})
 
 
 // Route - show all
-router.get("/", (req,res) => {
+router.get("/", async (req,res) => {
     res.render("animals/index")
 })
+
+
+// New
+router.get("/new", (req,res) => {
+    res.render("animals/new")
+})
+
 
 // Show
 router.get("/:id", (req,res) => {
     res.render("animals/show")
+})
+
+// Post 
+router.post("/", (req,res) => {
+    animals.push(req.body)
+    res.redirect("/animals")
 })
 
 
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // Dependencies
 const express = require("express");
 const app = express();
+const methodOverride = require('method-override');
+require("dotenv").config();
 
 // Middleware
 app.set("view engine", "ejs")
+app.use(methodOverride("_method")) // override for put and delete requests from forms
+app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
+app.use(express.static("public")) // serve files from public statically
+app.use(express.json());
+
 // Models
-// Controllers
 // Views
 
-require("dotenv").config();
-
+// Controllers
 const animalsController = require("./controllers/animals")
 app.use("/animals", animalsController)
 
